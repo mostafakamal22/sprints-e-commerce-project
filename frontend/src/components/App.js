073d20402cba4c6fb412bf8ea3cfcea686ac34ae
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2022-03-31 21:43:55 +0200

    added register page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router'
 import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
+        {/* test route (will be optimized later) */}
+        <Route path='/' element={<RegisterPage />} />
       </Routes>
     </div>
   )
