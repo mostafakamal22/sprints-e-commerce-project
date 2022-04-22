5cabc7f657c5ac52fad79094ed2aed9c3f498ab6
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-04-22 19:58:08 +0200

    adding images to product by admin

--- frontend/src/context/store/StoreContext.js
+++ frontend/src/context/store/StoreContext.js
@@ -2,168 +2,176 @@ import axios from "axios";
 import { createContext, useReducer } from "react";
 import storeReducer from "./StoreReducer";
 
-const StoreContext = createContext()
+const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
+  const initialState = {
+    auth: {
+      user: {
+        id: 0,
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        address1: "",
+        address2: "",
+        phone: "",
+        auth: 0,
+        status: 0,
+      },
+      token: "",
+      authed: false,
+    },
+    toast: {
+      isToast: false,
+      text: "",
+      isSuccess: true,
+    },
+    modal: {
+      isModal: false,
+    },
+    appData: {
+      branches: [],
+      brands: [],
+      products: [],
+      categories: [],
+    },
+    loading: false,
+  };
 
-    const initialState = {
-        auth: {
-            user: {
-                id: 0,
-                first_name: '',
-                last_name: '',
-                email: '',
-                password: '',
-                address1: '',
-                address2: '',
-                phone: '',
-                auth: 0,
-                status: 0,
-            },
-            token: '',
-            authed: false,
-        },
-        toast: {
-            isToast: false,
-            text: '',
-            isSuccess: true,
-        },
-        modal: {
-            isModal: false,
-        },
-        appData: {
-            branches: [],
-            brands: [],
-            products: [],
-            categories: [],
-        },
-        loading: false
-    }
+  const [store, dispatch] = useReducer(storeReducer, initialState);
 
-    const [store, dispatch] = useReducer(storeReducer, initialState)
+  const loginUser = (userData) => {
+    dispatch({
+      type: "LOGIN_USER",
+      payload: {
+        ...userData,
+        authed: true,
+      },
+    });
+  };
 
-    const loginUser = (userData) => {
-        dispatch({
-            type: 'LOGIN_USER',
-            payload: {
-                ...userData,
-                authed: true,
-            }
-        })
-    }
+  const logoutUser = () => {
+    dispatch({
+      type: "LOGOUT_USER",
+      payload: initialState,
+    });
+  };
 
-    const logoutUser = () => {
-        dispatch({
-            type: 'LOGOUT_USER',
-            payload: initialState,
-        })
-    }
+  const showToast = (text, isSuccess) => {
+    dispatch({
+      type: "SHOW_TOAST",
+      payload: {
+        isToast: true,
+        text,
+        isSuccess,
+      },
+    });
+  };
 
-    const showToast = (text, isSuccess) => {
-        dispatch({
-            type: 'SHOW_TOAST',
-            payload: {
-                isToast: true,
-                text,
-                isSuccess,
-            }
-        })
-    }
+  const hideToast = () => {
+    dispatch({
+      type: "HIDE_TOAST",
+      payload: {
+        isToast: false,
+      },
+    });
+  };
 
-    const hideToast = () => {
-        dispatch({
-            type: 'HIDE_TOAST',
-            payload: {
-                isToast: false
-            },
-        })
-    }
+  const showModal = (content) => {
+    const body = document.getElementById("body");
+    body.classList.toggle("overflow-hidden");
 
-    const showModal = (content) => {
-        
-        const body = document.getElementById('body')
-        body.classList.toggle('overflow-hidden')
+    dispatch({
+      type: "SHOW_MODAL",
+      payload: {
+        isModal: true,
+        content,
+      },
+    });
+  };
 
-        dispatch({
-            type: 'SHOW_MODAL',
-            payload: {
-                isModal: true,
-                content,
-            }
-        })
-    }
+  const hideModal = () => {
+    const body = document.getElementById("body");
+    body.classList.toggle("overflow-hidden");
 
-    const hideModal = () => {
+    dispatch({
+      type: "HIDE_MODAL",
+      payload: {
+        isModal: false,
+      },
+    });
+  };
 
-        const body = document.getElementById('body')
-        body.classList.toggle('overflow-hidden')
-        
-        dispatch({
-            type: 'HIDE_MODAL',
-            payload: {
-                isModal: false
-            },
-        })
-    }
+  const setAppData = async () => {
+    const brandsConfig = {
+      method: "get",
+      url: "https://mina-jpp1.herokuapp.com/api/brands",
+    };
+    const brands = await (await axios(brandsConfig)).data;
 
-    const setAppData = async () => {
-        const brandsConfig = {
-            method: 'get',
-            url: 'https://mina-jpp1.herokuapp.com/api/brands',
-        }
-        const brands = await (await axios(brandsConfig)).data
+    const categoriesConfig = {
+      method: "get",
+      url: "https://mina-jpp1.herokuapp.com/api/categories",
+    };
+    const categories = await (await axios(categoriesConfig)).data;
 
-        const categoriesConfig = {
-            method: 'get',
-            url: 'https://mina-jpp1.herokuapp.com/api/categories',
-        }
-        const categories = await (await axios(categoriesConfig)).data
+    const branchesConfig = {
+      method: "get",
+      url: "https://mina-jpp1.herokuapp.com/api/branches",
+    };
+    const branches = await (await axios(branchesConfig)).data;
 
-        const branchesConfig = {
-            method: 'get',
-            url: 'https://mina-jpp1.herokuapp.com/api/branches',
-        }
-        const branches = await (await axios(branchesConfig)).data
+    const productsConfig = {
+      method: "get",
+      url: "https://mina-jpp1.herokuapp.com/api/products",
+    };
+    const products = await (await axios(productsConfig)).data;
 
-        const productsConfig = {
-            method: 'get',
-            url: 'https://mina-jpp1.herokuapp.com/api/products',
-        }
-        const products = await (await axios(productsConfig)).data
+    //get all images from backend
+    const imagesConfig = {
+      method: "get",
+      url: "https://mina-jpp1.herokuapp.com/api/images",
+    };
 
-        dispatch({
-            type: 'SET_DATA',
-            payload: {
-                brands,
-                categories,
-                branches,
-                products,
-            }
-        })
-    }
+    const images = await (await axios(imagesConfig)).data;
 
-    const setLoading = (value) => {
-        dispatch({
-            type: 'SET_LOADING',
-            payload: value
-        })
-    }
+    dispatch({
+      type: "SET_DATA",
+      payload: {
+        brands,
+        categories,
+        branches,
+        products,
+        images,
+      },
+    });
+  };
 
-    return (
-        <StoreContext.Provider value={{
-            store,
-            logoutUser,
-            loginUser,
-            showToast,
-            hideToast,
-            showModal,
-            hideModal,
-            setAppData,
-            setLoading,
-        }}>
-            {children}
-        </StoreContext.Provider>
-    )
-}
+  const setLoading = (value) => {
+    dispatch({
+      type: "SET_LOADING",
+      payload: value,
+    });
+  };
 
-export default StoreContext
+  return (
+    <StoreContext.Provider
+      value={{
+        store,
+        logoutUser,
+        loginUser,
+        showToast,
+        hideToast,
+        showModal,
+        hideModal,
+        setAppData,
+        setLoading,
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContext;
